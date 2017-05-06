cbab43b9590c5bc3db886e4c24e3f4c41eee7d4c
Nathan Grey <Nathan@NathanGrey.com>
2017-05-05 18:23:20 -0700

    Update New.cpp

--- New.cpp
+++ New.cpp
@@ -1,5 +1,4 @@
 #include <iostream> // Includes the preprocessors
-
 using namespace std; // Declares the namespace
 
 int FunctionOne(void* pPointer, int size) // pPointer type is void and size type of int
@@ -28,73 +27,91 @@ void Swap2(int &pFirstNumber, int &pSecondNumber) // References the points to as
 void To1337Speak(char *pString)
 {
 	char* ptr = pString;
+
 	while (*ptr != '\n')
 	{
 		switch (*ptr)
 		{
 			case 'a':
 			case 'A':
+
 				*ptr = '4';
-				break;
+					break;
+
 			case 'e':
 			case 'E':
+
 				*ptr = '3';
 				break;
+
 			case 't':
 			case 'T':
+
 				*ptr = '7';
+				break;
+
 			case 'i':
 			case 'I':
+
 				*ptr = '!';
 				break;
+
 			case 'l':
 			case 'L':
+
 				*ptr = '1';
 				break;
+
 			case 'o':
 			case 'O':
+
 				*ptr = '0';
 				break;
+
 			case 'g':
 			case 'G':
+
 				*ptr = '9';
 				break;
+
 			case 's':
 			case 'S':
+
 				*ptr = '5';
 				break;
+
 			default:
 				break;
 		}
-			++ptr;
+			++ptr; // Make the char increment by one due to the memory address
 
 	}
 }
 
-int main()
+int main() // Main program
 {
 
 	char* aPartOne = nullptr; // Points to char
 	int* pPartTwo = nullptr; // Points to int
 
-	int a = 7;
+	int a = 7; // Declares the chars as numbers
 	int b = 3;
 
-	Swap(&a, &b);
+	Swap(&a, &b); // Initiates the Swap function
 
-	cout << a << " " << b << endl;
+	cout << a << "\n" << b << endl; // Prints the swapped values
 
-    char someString[] = "Leaning my pointers and references";
-    To1337Speak(someString);
-    cout << someString << endl << endl;
+    char someString[] = "Leaning my pointers and references"; // String to be converted
+    To1337Speak(someString); // Calls the function to translate the string
+    cout << someString << endl << endl; // Prints the results
 
-    int Number = 20;
-    int *pPointerToNumber = &Number;
+    int Number = 20; // Declares a variable
+    int *pPointerToNumber = &Number; // Makes a pointer to reference it
 
     cout << "The value of Number is: " << Number << endl;
     cout << "But the address of Number is " << pPointerToNumber << endl;
     cout << "And this will derference it: " << *pPointerToNumber << endl;
-    *pPointerToNumber = 10;
+    *pPointerToNumber = 10; // Dereferencing to change the previously set value
     cout << "We can also assign a new value via derefence which is: " << Number << endl;
 
 system("pause");
